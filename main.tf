05861fa0adbbbf96b367a3bf7b3e032d838b0ea8
aniketbadadhe49-devops <aniketbadadhe49@gmail.com>
2025-11-10 23:01:46 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,178 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
+
+  required_version = ">= 1.5.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+# 1️⃣ Resource Group
+resource "azurerm_resource_group" "rg" {
+  name     = "${var.prefix}-rg"
+  location = var.location
+}
+
+# 2️⃣ Virtual Network + Subnets
+resource "azurerm_virtual_network" "vnet" {
+  name                = "${var.prefix}-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "subnet_vm" {
+  name                 = "subnet-vm"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_subnet" "subnet_appgw" {
+  name                 = "subnet-appgw"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+# 3️⃣ Public IP for Load Balancer & Application Gateway
+resource "azurerm_public_ip" "lb_pip" {
+  name                = "${var.prefix}-lb-pip"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+resource "azurerm_public_ip" "appgw_pip" {
+  name                = "${var.prefix}-appgw-pip"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+# 4️⃣ Load Balancer
+resource "azurerm_lb" "lb" {
+  name                = "${var.prefix}-lb"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku                 = "Standard"
+
+  frontend_ip_configuration {
+    name                 = "PublicFrontend"
+    public_ip_address_id = azurerm_public_ip.lb_pip.id
+  }
+}
+
+# 5️⃣ Backend Address Pool
+resource "azurerm_lb_backend_address_pool" "bepool" {
+  loadbalancer_id = azurerm_lb.lb.id
+  name            = "BackendPool"
+}
+
+# 6️⃣ VM Scale Set (2 instances)
+resource "azurerm_linux_virtual_machine_scale_set" "vmss" {
+  name                = "${var.prefix}-vmss"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Standard_B1s"
+  instances           = 2
+  admin_username      = "azureuser"
+  admin_password      = "P@ssword1234!"
+  upgrade_mode        = "Manual"
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+
+  network_interface {
+    name    = "nic"
+    primary = true
+
+    ip_configuration {
+      name                                   = "internal"
+      primary                                = true
+      subnet_id                              = azurerm_subnet.subnet_vm.id
+      load_balancer_backend_address_pool_ids = [azurerm_lb_backend_address_pool.bepool.id]
+    }
+  }
+}
+
+# 7️⃣ Application Gateway
+resource "azurerm_application_gateway" "appgw" {
+  name                = "${var.prefix}-appgw"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  sku {
+    name     = "WAF_v2"
+    tier     = "WAF_v2"
+    capacity = 2
+  }
+
+  gateway_ip_configuration {
+    name      = "gwipconfig"
+    subnet_id = azurerm_subnet.subnet_appgw.id
+  }
+
+  frontend_port {
+    name = "port80"
+    port = 80
+  }
+
+  frontend_ip_configuration {
+    name                 = "frontend"
+    public_ip_address_id = azurerm_public_ip.appgw_pip.id
+  }
+
+  backend_address_pool {
+    name = "backendPool"
+  }
+
+  backend_http_settings {
+    name                  = "httpSettings"
+    cookie_based_affinity = "Disabled"
+    port                  = 80
+    protocol              = "Http"
+    request_timeout       = 20
+  }
+
+  http_listener {
+    name                           = "listener"
+    frontend_ip_configuration_name = "frontend"
+    frontend_port_name             = "port80"
+    protocol                       = "Http"
+  }
+
+  request_routing_rule {
+    name                       = "rule1"
+    rule_type                  = "Basic"
+    http_listener_name         = "listener"
+    backend_address_pool_name  = "backendPool"
+    backend_http_settings_name = "httpSettings"
+  }
+}
+
+# 8️⃣ DNS Zone + Record
+resource "azurerm_dns_zone" "dns_zone" {
+  name                = var.dns_zone_name
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_dns_a_record" "dns_record" {
+  name                = var.dns_record_name
+  zone_name           = azurerm_dns_zone.dns_zone.name
+  resource_group_name = azurerm_resource_group.rg.name
+  ttl                 = 300
+  records             = [azurerm_public_ip.appgw_pip.ip_address]
+}
