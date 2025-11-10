6bd2324bcafcfc733456ad9693896000f2517ac3
aniketbadadhe49-devops <aniketbadadhe49@gmail.com>
2025-11-11 01:22:03 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -18,3 +18,7 @@ variable "dns_record_name" {
   type        = string
   default     = "app"
 }
+variable "ssh_public_key" {
+  description = "SSH public key for VM access"
+  type        = string
+}
